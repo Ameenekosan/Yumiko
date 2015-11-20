a981688cb41fd5185038295be9622b55a4bf0084
Ameenekosan <amelie.f.lemieux@gmail.com>
2015-11-19 19:15:20 -0500

    changed stuff . non working state .. need to be fixed

--- sopel/modules/uguuript.py
+++ sopel/modules/uguuript.py
@@ -2,6 +2,9 @@
 #plugins thats worked on Uguubot
 
 from sopel.module import commands, rule, priority, example
+import random
+from random import randint
+import json
 
 #hack
 @commands("hack")
@@ -12,3 +15,12 @@ def hacking(bot, trigger):
         bot.action("is hacking %s" % nickname)
     else :
         bot.say("Hacking everyone...")
+
+#8ball
+@commands("8ball")
+with open("modules/data/8ball_responses.txt") as f:
+    responses = [line.strip() for line in f.readlines() if not line.startswith("//")]
+def heightball(bot,trigger):
+    if trigger.goup(2):
+        bot.action(shakes the magic ball)
+        bot.say(random.choice(responses))
